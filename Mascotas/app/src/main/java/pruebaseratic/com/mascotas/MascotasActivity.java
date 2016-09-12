655c79b8f2b3a791ed280b6ee0875799d0576e17
victacora <victoralfonsopuyo@gmail.com>
2016-09-12 08:59:57 -0500

    Crear repositorio
    Estructura General

---
+++ Mascotas/app/src/main/java/pruebaseratic/com/mascotas/MascotasActivity.java
@@ -0,0 +1,13 @@
+package pruebaseratic.com.mascotas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MascotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mascotas);
+    }
+}
